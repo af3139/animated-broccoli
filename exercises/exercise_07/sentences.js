4ff603afc02b7d54d9d7cf14b8c322b2b6086d43
af3139 <97997004+af3139@users.noreply.github.com>
2022-03-04 16:30:56 -0500

    update landing page

---
+++ exercises/exercise_07/sentences.js
@@ -0,0 +1,7 @@
+let noun = "cat";
+let verb = "see";
+let adjective = "beautifully";
+
+let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`
+
+document.write(sentence)
