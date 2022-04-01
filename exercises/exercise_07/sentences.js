f1f334d71f63f3b6b0a4a56146c2b80c1f379a38
af3139 <97997004+af3139@users.noreply.github.com>
2022-04-01 13:09:27 -0400

    exercise 07

--- exercises/exercise_07/sentences.js
+++ exercises/exercise_07/sentences.js
@@ -1,7 +1,15 @@
-let noun = "cat";
-let verb = "see";
-let adjective = "beautifully";
+let noun = ["box", "circle", "sphere", "column", "cabinet"]
+let adjective1 = ["lavender", "magenta", "cyan", "violet", "lilac"]
+let adjective2 = ["cornerless", "pristine", "dusty", "childlike", "wrinkled"]
+let verb = ["rattles", "trembles", "shatters", "melts", "stretches"]
+let adjective3 = ["angry", "happy", "morose", "petulant", "pretentious"]
 
-let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`
+const RandomNoun = Math.floor(Math.random() * noun.length);
+const RandomAdjective1 = Math.floor(Math.random() * adjective1.length);
+const RandomAdjective2 = Math.floor(Math.random() * adjective2.length);
+const RandomVerb = Math.floor(Math.random() * verb.length);
+const RandomAdjective3 = Math.floor(Math.random() * adjective3.length);
+
+let sentence = `sometimes in its ${RandomNoun, noun[RandomNoun]} of sky ${RandomAdjective1, adjective1[RandomAdjective1]} and ${RandomAdjective2, adjective2[RandomAdjective2]}, the moon ${RandomVerb, verb[RandomVerb]} like a fragment of ${RandomAdjective3, adjective3[RandomAdjective3]} candy`
 
 document.write(sentence)
